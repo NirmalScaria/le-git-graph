2ace8e4d24fc2f18a4f75809c87dbeb5613a810e
Nirmal Scaria <46727865+NirmalScaria@users.noreply.github.com>
2022-08-11 11:55:46 +0530

    [UI] Branches Indicator

--- js/showLegend.js
+++ js/showLegend.js
@@ -1,4 +1,14 @@
 function showLegend(heads) {
-    console.log(heads);
-    console.log("Are the heads");
+    var legendContainer = document.getElementById("legendContainer");
+    var branchButton = legendContainer.querySelector("#branchButton").cloneNode(true);
+    legendContainer.innerHTML = "";
+    for(var head of heads) {
+        var newBranch = branchButton.cloneNode(true);
+        newBranch.querySelector("#branchName").innerHTML = head.name;
+        var color = document.querySelector('[circlesha="' + head.oid + '"]').getAttribute("fill");
+        newBranch.querySelector("#insideCircle").setAttribute("fill", color);
+        newBranch.querySelector("#outsideCircle").setAttribute("stroke", color);
+        legendContainer.appendChild(newBranch);
+    }
+    legendContainer.style.display = "block";
 }
